aad6e887f46f36e380452aff690d9450ac504a40
Caffleck16 <caiden.affleck@ucalgary.ca>
2023-03-07 18:48:10 -0700

    Base project files (added styles and app outline)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./Layout";
+import BlankNote from "./BlankNote";
+import Note from "./Note";
+import Display from "./Display";
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <div className="notes">
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element= {<BlankNote />}></Route>
+              <Route path="/notes" element= {<BlankNote />}></Route>
+              <Route path="/notes/:noteID/edit" element={<Note />}></Route>
+              <Route path="/notes/:noteID" element= {<Display />}></Route>
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
