3ec72eb17bd89b47e0723f1e4c8fdca663029953
Caffleck16 <caiden.affleck@ucalgary.ca>
2023-03-07 18:48:39 -0700

    added file for blank-notes

---
+++ src/BlankNote.js
@@ -0,0 +1,9 @@
+const BlankNote = () => {
+    return ( 
+        <div className="blank-note">
+            <h3>Select a note, or create a new one</h3>
+        </div>
+     );
+}
+ 
+export default BlankNote;
